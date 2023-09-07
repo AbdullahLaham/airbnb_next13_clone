6e1d10097d71d42acc4aba7d548a8c33ba7eba68
AbdullahLaham <abed26194@gmail.com>
2023-09-07 15:55:27 +0300

    changes

--- app/components/modals/SearchModal.tsx
+++ app/components/modals/SearchModal.tsx
@@ -2,13 +2,113 @@
 
 
 import useSearchModal from '@/app/hooks/useSearchModal'
-import React from 'react'
+import React, { useCallback, useMemo, useState } from 'react'
 import Modal from './Modal';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { Range } from 'react-date-range';
+import dynamic from 'next/dynamic';
+import CountrySelect, { CountrySelectValue } from '../inputs/CountrySelect';
+import qs from 'query-string';
+import { formatISO, setDate } from 'date-fns';
+import Heading from '../Heading';
+import Calendar from '../inputs/Calendar';
+import Counter from '../inputs/Counter';
+
+enum STEPS {
+  LOCATION = 0,
+  DATE = 1,
+  INFO = 2,
+}
 
 const SearchModal = () => {
     const searchModal = useSearchModal();
+    const router = useRouter();
+    const params = useSearchParams();
+    const [location, setLocation] = useState<CountrySelectValue>();
+  const [step, setStep] = useState(STEPS.LOCATION);
+  const [guestCount, setGuestCount] = useState(1);
+  const [roomCount, setRoomCount] = useState(1);
+  const [bathroomCount, setBathroomCount] = useState(1);
+  const [dateRange, setDateRange] = useState<Range>({
+    startDate: new Date(),
+    endDate: new Date(),
+    key: 'selection',
+  });
+  const Map = useMemo(() => dynamic(() => import('../Map'), { ssr: false }), [location]) // no server side rendering
+  const onBack = useCallback(() => {
+    setStep((value) => value - 1);
+  }, [])
+  const onNext = useCallback(() => {
+      setStep((value) => value + 1);
+  }, [])
+  const onSubmit = useCallback((async() => {
+    if (step !== STEPS.INFO) {
+      return onNext();
+    }
+    let currentQuery = {};
+    if (params) currentQuery = qs.parse(params.toString());
+    const updatedQuery: any = {
+      ...currentQuery,
+      locationValue: location?.value,
+      guestCount,
+      roomCount,
+      bathroomCount,
+    }
+    if (dateRange.startDate) updatedQuery.startDate = formatISO(dateRange.startDate)
+
+    if (dateRange.endDate) updatedQuery.endDate = formatISO(dateRange.endDate)
+
+    const url = qs.stringifyUrl({
+      url: '/',
+      query: updatedQuery
+    }, {skipNull: true});
+    setStep(STEPS.LOCATION);
+    searchModal.onClose();
+    router.push(url);
+
+  }), [step, searchModal, location, router, guestCount, roomCount, bathroomCount, dateRange, onNext, params]);
+  
+  const actionLabel = useMemo(() => {
+    if (step !== STEPS.INFO) {
+      return 'Next'
+    }
+    return 'Search';
+  }, [step])
+
+  const secondaryAtionLabel = useMemo(() => {
+    if (step !== STEPS.LOCATION) {
+      return 'Back'
+    }
+    return undefined;
+  }, [step])
+  
+  let bodyContent = (
+    <div className='flex flex-col gap-8'>
+      <Heading title='were do you wanna go!' subtitle='find the perfect location!' />
+      <CountrySelect value={location} onChange={(value) => setLocation(value as CountrySelectValue)}  />
+      <Map center={location?.latlng} />
+    </div>
+  )
+  if (step == STEPS.DATE) {
+    bodyContent = (
+      <div className='flex flex-col gap-8'>
+        <Heading title='when do you plan to go!' subtitle='Make sure anyone is free !' />
+        <Calendar value={dateRange} onChange={(value) => setDateRange(value.selection)} />
+      </div>
+    )
+  }
+  if (step == STEPS.INFO) {
+    bodyContent = (
+      <div className='flex flex-col gap-8'>
+        <Heading title='More Information' subtitle='Find your perfect place !' />
+        <Counter title='Guests' subtitle='How many are comming ? ' onChange={(value) => setGuestCount(value)} value={guestCount} />
+        <Counter title='Rooms' subtitle='How many rooms do you need ? ' onChange={(value) => setRoomCount(value)} value={roomCount} />
+        <Counter title='Bathrooms' subtitle='How many bathrooms do you need ? ' onChange={(value) => setBathroomCount(value)} value={bathroomCount} />
+      </div>
+    )
+  }
   return (
-    <Modal isOpen={searchModal.isOpen} onClose={searchModal.onClose} onSubmit={searchModal.onOpen} title='Filters' actionLabel='Search'  />
+    <Modal isOpen={searchModal.isOpen} onClose={searchModal.onClose} onSubmit={onSubmit} title='Filters' actionLabel='Search' body={bodyContent} secondaryActionLabel={secondaryAtionLabel} secondaryAction={step === STEPS.LOCATION ? undefined : onBack} actionLabel={actionLabel} body={bodyContent}   />
         
   )
 }
---
+++ app/loading.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+
+
+import React from 'react'
+import Loader from './components/Loader'
+
+const loading = () => {
+  return (
+    <Loader />
+  )
+}
+
+export default loading
