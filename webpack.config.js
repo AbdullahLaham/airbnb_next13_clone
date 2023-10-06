7225a856663a6883d67e59cced1952c455b87018
AbdullahLaham <abed26194@gmail.com>
2023-10-06 23:21:50 +0300

    ch

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack'); //to access built-in plugins
+const path = require('path');
 
 module.exports = {
+  output: {
+    filename: 'my-first-webpack.bundle.js',
+  },
   module: {
     rules: [{ test: /\.txt$/, use: 'raw-loader' }],
   },
-  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
 };
