06de9bcbacc5f5093af156c8a071f161c159ace9
AbdullahLaham <abed26194@gmail.com>
2023-10-06 23:07:45 +0300

    ch

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    webpack: (config, options) => {
+        config.module.rules.push({
+          test: /\.mdx/,
+          use: [
+            options.defaultLoaders.babel,
+            {
+              loader: '@mdx-js/loader',
+              options: pluginOptions.options,
+            },
+          ],
+        })
+     
+        return config
+      },
     images: {
         domains: [
             "res.cloudinary.com",
